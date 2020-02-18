6257a168217e3f4c1fcd3a2e6f9818ffac2d8b23
root <root@master.vagrant.vm>
2020-02-18 18:09:55 +0000

    Baseline

--- src/com/dcc/ComponentInfo.groovy
+++ src/com/dcc/ComponentInfo.groovy
@@ -6,4 +6,54 @@ class ComponentInfo {
   String gitPath;
   String folderPath;
   String dockerRepo;  
+
+  void setName(String pname) {
+    name = pname;
+  }
+
+  String getName() {
+    return this.name;
+  }
+
+  void setShortName(String pshortName) {
+    shortName = pshortName;
+  }
+
+  String getShortName() {
+    return this.shortName;
+  }
+
+  void setTag(String ptag) {
+    tag = ptag;
+  }
+
+  String getTag() {
+    return this.tag;
+  }
+
+  void setGitPath(String pgitPath) {
+    gitPath = pgitPath;
+  }
+
+  String getGitPath() {
+    return this.gitPath;
+  }
+
+  void setFolderPath(String pfolderPath) {
+    folderPath = pfolderPath;
+  }
+
+  String getFolderPath() {
+    return this.folderPath;
+  }
+
+  void setDockerRepo(String pdockerRepo) {
+    dockerRepo = pdockerRepo;
+  }
+
+  String getDockerRepo() {
+    return this.dockerRepo;
+  }
+
+
 }
