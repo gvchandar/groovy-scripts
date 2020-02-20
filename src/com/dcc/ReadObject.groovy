984e6cea11c545be79e5838a33e5e43a958b8972
root <root@master.vagrant.vm>
2020-02-20 16:18:14 +0000

    Basline

---
+++ src/com/dcc/ReadObject.groovy
@@ -0,0 +1,23 @@
+//src/com/dcc/DTRBuildInfo.groovy
+package com.dcc
+import java.io.File
+class ReadObject 
+{
+  ReadObject(String fileName) {
+    File objectStore = new File(fileName)
+    assert  objectStore.exists()
+    def is
+    try {
+      is = objectStore.newObjectInputStream(this.class.classLoader)
+      is.eachObject { 
+        println it 
+      }
+    } 
+    catch (e) { 
+      throw new Exception(e) 
+    } 
+    finally { 
+      is?.close() 
+    }   
+  }
+}
