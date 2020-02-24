cf722f838444303d89e6bdd86e2c7128f4cba757
root <root@master.vagrant.vm>
2020-02-24 14:17:06 +0000

    Basline

--- DTRBuildSer.groovy
+++ DTRBuildSer.groovy
@@ -1,23 +1,25 @@
 import groovy.json.JsonOutput 
 import groovy.json.JsonSlurper
+import groovy.json.JsonParserType
 class DTRBuildSer {
   static void main(String[] args) {
+    /*
     def output = JsonOutput.toJson([name: 'John', ID: 1])
     println(output);  
     
-    //Convert Object to JSON
+    Convert Object to JSON
     def json = JsonOutput.toJson([ new Person(name: 'John',interests: ["a","b","c"]), \
                                    new Person(name: 'Max' ,interests: ["d","e","f"]) ])
-    //assert json == '[{"name":"John"},{"name":"Max"}]'
+    assert json == '[{"name":"John"},{"name":"Max"}]'
     JsonOutput.prettyPrint(json)
     println(json);
-    //Convert JSON to Object
+    Convert JSON to Object
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(json)
     println(object);
-
-    //def cleanup = new ComponentInfo(name:"cleanup",tag: "5.2",gitPath:"gitPathcleanup")
-    //def manager = new ComponentInfo(name:"manager",tag: "5.4",gitPath:"gitPathmanager")
+    */
+    def cleanup = new ComponentInfo(name:"cleanup",tag: "5.2",gitPath:"gitPathcleanup")
+    def manager = new ComponentInfo(name:"manager",tag: "5.4",gitPath:"gitPathmanager")
     
     def cleanupmap= [
         'name': 'dco-cleanup',
@@ -30,18 +32,40 @@ class DTRBuildSer {
         'subversion': '2.8',
         'tag': 'auto-clean'
     ]
+    //def dtrbuild = new DTRBuildInfo(environment:"DEV",version:"5.9",\
+    //                   componentList:[cleanupmap,requestmanagermap])
     def dtrbuild = new DTRBuildInfo(environment:"DEV",version:"5.9",\
-                       componentList:[cleanupmap,requestmanagermap])
+                       componentList:[cleanup,manager])
     def dtrbuild_json = JsonOutput.toJson(dtrbuild)
-    println(" Print JSON")
-    println(dtrbuild_json)
-    def dtrbuild_object = jsonSlurper.parseText(dtrbuild_json)
-    //assert dtrbuild_object instanceof DTRBuildInfo
+    //println(" Print JSON")
+    //println(dtrbuild_json)
+
+
+    JsonSlurper slurper = new JsonSlurper().setType( JsonParserType.INDEX_OVERLAY )
+    def reloadobject = slurper.parseText(dtrbuild_json)
+    DTRBuildInfo reloadDTRBuildInfo = new DTRBuildInfo(reloadobject)
+
     println(" Print Object")
-    //println(dtrbuild_object)
+    println("Environment $reloadDTRBuildInfo.environment")
+    println("Version $reloadDTRBuildInfo.version")
+    println("Component List $reloadDTRBuildInfo.componentList")
+    
+    reloadDTRBuildInfo.componentList.each {
+      println"Object is:"
+      println "${it}"
+      def componentSplit = "${it}".split(',');   
+      for( String values : componentSplit )
+      println(values);
+
+    }
+    
+
+    //assert dtrbuild_object instanceof DTRBuildInfo
+    /*
+    println(dtrbuild_object)
     println(dtrbuild_object.environment)
     println(dtrbuild_object.version)
-    //println(dtrbuild_object.componentList)
+    println(dtrbuild_object.componentList)
     def componentmap = [:]
     dtrbuild_object.componentList.each {
       componentmap = "${it}"
@@ -50,7 +74,7 @@ class DTRBuildSer {
         println "${it}"
       }
     }
-
+    */
     //println("version is :"dtrbuild_object.version)
     //println("version is :" dtrbuild_object.getVersion())
     //println("component list is :" dtrbuild_object.componentList)
