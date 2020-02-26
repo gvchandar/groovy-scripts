66abce0ae93378b389cdf8b513194109f260c86d
root <root@master.vagrant.vm>
2020-02-26 20:02:06 +0000

    Basline

--- DTRSerializeManual.groovy
+++ DTRSerializeManual.groovy
@@ -18,55 +18,93 @@ class DTRSerializeManual {
    'folderPath': 'managerFolder', \
    'dockerRepo': 'http://managerdockerrepo'
   )
-  def dtrbuild = new DTRBuildInfo(environment: "DEV", version: "5.9", \
-   componentList: [cleanup, manager])
-
-
+  def dtrBuildInfo = new DTRBuildInfo(environment: "DEV", version: "5.9", \
+                 componentList: [cleanup, manager])
+  String dtrBuildInfoString =  getDTRBuildInfoString(dtrBuildInfo)
+  println("dtrBuildInfoString"+ dtrBuildInfoString)
+  DTRBuildInfo reconstructDTRBuildInfo = getDTRBuildInfoObject(dtrBuildInfoString)
 
  }
 
-    public String GetBuildStringParam(DTRBuildInfo build)
-    {
-        String buildStringParam = "";
-        buildStringParam + "environment~" + build.environment + "|";
-        buildStringParam + "version~" + build.version + "|";
-        buildStringParam + "DTRURL~" + build.DTRURL + "|";
+ public static String getDTRBuildInfoString(DTRBuildInfo build) {
+  StringBuffer buildStringParam = new StringBuffer();
+  buildStringParam.append('environment~' + build.environment + '^')
+  buildStringParam.append('version~' + build.version + '^')
+  buildStringParam.append('DTRURL~' + build.DTRURL + '^')
+  buildStringParam.append('£')
+  println("build.componentList" + build.componentList)
+  build.componentList.eachWithIndex {component,i ->
+   assert component instanceof ComponentInfo
+   buildStringParam.append(getComponentInfoString(component))
+  }
+  return buildStringParam.toString()
+ }
 
-        buildStringParam + "£";
+ public static String getComponentInfoString(ComponentInfo component) {
+  StringBuffer componentInfoStringParam = new StringBuffer();
+  componentInfoStringParam.append('name~' + component.name + '^')
+  componentInfoStringParam.append('shortName~' + component.shortName + '^')
+  componentInfoStringParam.append('subVersion~' + component.subversion + '^')
+  componentInfoStringParam.append('tag~' + component.tag + '^')
+  componentInfoStringParam.append('gitPath~' + component.gitPath + '^')
+  componentInfoStringParam.append('folderPath~' + component.folderPath + '^')
+  componentInfoStringParam.append('dockerRepo~' + component.dockerRepo + '`')
+  println(componentInfoStringParam.toString())
+  return componentInfoStringParam.toString()
+ }
 
-        foreach (var item in build.components)
-        {
-            buildStringParam += getComponentInfoStringParam(item) + "`";
-        }
-        buildStringParam += "|";
 
+ public static DTRBuildInfo getDTRBuildInfoObject(String buildParam) {
+  DTRBuildInfo dtrBuildInfo = new DTRBuildInfo();
+  String[] buildParamStringArray = buildParam.split('£')
+  String DTRBuildInfoString = buildParamStringArray[0]
+  String ComponentInfoString = buildParamStringArray[1]
+  println("DTRBuildInfoString" + DTRBuildInfoString)
+  println("ComponentInfoString" + ComponentInfoString)
+  String[] DTRBuildInfoArray =  DTRBuildInfoString.split('\\^')
+  dtrBuildInfo.environment = DTRBuildInfoArray[0].split('~')[1]
+  dtrBuildInfo.version = DTRBuildInfoArray[1].split('~')[1]
+  dtrBuildInfo.DTRURL = DTRBuildInfoArray[2].split('~')[1]
 
-        return buildStringParam;
-    }
+  println("dtrBuildInfo.environment " + dtrBuildInfo.environment)
+  println("dtrBuildInfo.version " + dtrBuildInfo.version)
+  println("dtrBuildInfo.DTRURL " + dtrBuildInfo.DTRURL)
 
+  String[] ComponentInfoArray = ComponentInfoString.split('\\`')
+  for( String values : ComponentInfoArray ) {
+    ComponentInfo component =  getComponentInfoObject(values)
+    println("component.name " + component.name)
+    println("component.shortName " + component.shortName)
+    println("component.subversion " + component.subversion)    
+    println("component.tag " + component.tag)
+    println("component.gitPath " + component.gitPath)
+    println("component.folderPath " + component.folderPath)     
+    println("component.dockerRepo " + component.dockerRepo)     
 
+  }
+  return dtrBuildInfo
+ }
 
-
- public static String  getComponentInfoStringParam(ComponentInfo component) {
-   String componentStringParam = "";
-   componentStringParam + "name~" + component.name + "|";
-   componentStringParam + "shortName~" + component.shortName + "|";
-   componentStringParam + "subVersion~" + component.subVersion + "|";
-   componentStringParam + "tag~" + component.tag + "|";
-   componentStringParam + "gitPath~" + component.gitPath + "|";
-   componentStringParam + "folderPath~" + component.folderPath+ "|";
-   componentStringParam + "dockerRepo~" + component.dockerRepo+ "|";
-   return componentStringParam;
-   }
-
+ public static ComponentInfo getComponentInfoObject(String buildParams) {
+  ComponentInfo buildComponent = new ComponentInfo();
+  String[] ComponentInfoArray =  buildParams.split('\\^');
+  buildComponent.name = ComponentInfoArray[0].split('~')[1];
+  buildComponent.shortName = ComponentInfoArray[1].split('~')[1];
+  buildComponent.subversion = ComponentInfoArray[2].split('~')[1];
+  buildComponent.tag = ComponentInfoArray[3].split('~')[1];
+  buildComponent.gitPath = ComponentInfoArray[4].split('~')[1];
+  buildComponent.folderPath = ComponentInfoArray[5].split('~')[1];
+  buildComponent.dockerRepo = ComponentInfoArray[6].split('~')[1];
+  return buildComponent;
+ }
 }
 
 class DTRBuildInfo {
  String environment;
  String version;
  String DTRURL;
- def componentList = [];
-}
+ ComponentInfo[] componentList = new ComponentInfo[10];
+ }
 
 class ComponentInfo {
  String name;
@@ -77,4 +115,3 @@ class ComponentInfo {
  String folderPath;
  String dockerRepo;
 }
-
