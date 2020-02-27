3c1168133c58f1369d78c2df67944759a91ea669
root <root@master.vagrant.vm>
2020-02-27 12:08:06 +0000

    Basline

--- src/com/dcc/ComponentInfo.groovy
+++ src/com/dcc/ComponentInfo.groovy
@@ -1,73 +1,11 @@
 package com.dcc
 
-class ComponentInfo 
-{
+class ComponentInfo {
   String name;
   String shortName;
   String subversion;
   String tag;
   String gitPath;
   String folderPath;
-  String dockerRepo;  
-
-  ComponentInfo(name, subversion,tag) {          
-        this.name = name
-        this.shortName = null
-        this.subversion = subversion
-        this.tag = tag
-        this.gitPath = null
-        this.tag = tag
-        this.dockerRepo = null
-    }
-
-
-  void setName(String pname) {
-    name = pname;
-  }
-
-  String getName() {
-    return this.name;
-  }
-
-  void setShortName(String pshortName) {
-    shortName = pshortName;
-  }
-
-  String getShortName() {
-    return this.shortName;
-  }
-
-  void setTag(String ptag) {
-    tag = ptag;
-  }
-
-  String getTag() {
-    return this.tag;
-  }
-
-  void setGitPath(String pgitPath) {
-    gitPath = pgitPath;
-  }
-
-  String getGitPath() {
-    return this.gitPath;
-  }
-
-  void setFolderPath(String pfolderPath) {
-    folderPath = pfolderPath;
-  }
-
-  String getFolderPath() {
-    return this.folderPath;
-  }
-
-  void setDockerRepo(String pdockerRepo) {
-    dockerRepo = pdockerRepo;
-  }
-
-  String getDockerRepo() {
-    return this.dockerRepo;
-  }
-
-
+  String dockerRepo;
 }
--- src/com/dcc/DTRBuildInfo.groovy
+++ src/com/dcc/DTRBuildInfo.groovy
@@ -1,42 +1,9 @@
-//src/com/dcc/DTRBuildInfo.groovy
 package com.dcc
-import groovy.json.JsonOutput
 
-// DTR Build Information
-class DTRBuildInfo 
+class DTRBuildInfo
 {
   String environment;
   String version;
   String DTRURL;
-  def componentList = []
-
-  void setEnvironment(String pEnvironment) {
-    environment = pEnvironment;
-  }
-	
-  String getEnvironment() {
-    return this.environment;
-  }
-
-  void setVersion(String pVersion) {
-    version = pVersion;
-  }
-	
-  String getVersion() {
-    return this.version;
-  }
-
-  void addComponent(ComponentInfo component){
-    componentList.add(component)
-  }
-
-  static String getJSON() {
-    def output = JsonOutput.toJson([name: 'John', ID: 1])
-    return output
-  }
-  static String getJSON(DTRBuildInfo dtrbuild) {
-    def output = JsonOutput.toJson(dtrbuild)
-    return output
-  }
-  
+  ComponentInfo[] componentList = new ComponentInfo[10];
 }
