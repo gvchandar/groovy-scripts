9776a8771ed8a02da9cbe4b04666997cef338825
root <root@master.vagrant.vm>
2020-02-27 14:32:53 +0000

    Basline

---
+++ WriteObject.groovy
@@ -0,0 +1,27 @@
+//src/com/dcc/DTRBuildInfo.groovy
+package com.dcc
+import java.io.File
+
+// DTR Build Information
+class WriteObject 
+{
+  WriteObject(DTRBuildInfo DTRBuild, String fileName) {
+    File objectStore = new File(fileName)
+    if (objectStore.exists()) { objectStore.delete() }
+    assert ! objectStore.exists()
+    def os
+    try {
+      os = objectStore.newObjectOutputStream()
+      os << DTRBuild
+    }
+    catch (e) { 
+      throw new Exception(e) 
+    }
+    finally { 
+     os?.close() 
+     println(" FINALLY of WriteObject")
+
+    }
+    assert objectStore.exists()    
+  }
+}
